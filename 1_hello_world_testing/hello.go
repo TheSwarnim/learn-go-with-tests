fb3f14dcafa7c9ab442a20f2ca279853e8dd183f
Swarnim Pratap Singh <54211377+TheSwarnim@users.noreply.github.com>
2023-06-03 15:32:12 +0530

    added hello world test

---
+++ 1_hello_world_testing/hello.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func hello() string {
+	return "Hello, World!"
+}
+
+func main() {
+	// It is good to separate your "domain" code from the outside world (side-effects).
+	// The fmt.Println is a side effect (printing to stdout) and the string we send in
+	// is our domain. 
+	// fmt.Println("Hello, World!")
+
+	// so I've created a function called hello() that returns a string
+	// and I'm calling that function here
+	fmt.Println(hello())
+}
