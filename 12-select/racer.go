b1eacf0f0b26430a828c07957ea35aae9a3b2885
Swarnim Pratap Singh <54211377+TheSwarnim@users.noreply.github.com>
2023-07-20 13:42:32 +0530

    completed 12-select

--- 12-select/racer.go
+++ 12-select/racer.go
@@ -1,24 +1,55 @@
 package racer
 
 import (
+	"fmt"
 	"net/http"
 	"time"
 )
 
-func Racer(a, b string) string {
-	aDuration := measureResponseTime(a)
+// func Racer(a, b string) string {
+// 	aDuration := measureResponseTime(a)
 
-	bDuration := measureResponseTime(b)
+// 	bDuration := measureResponseTime(b)
 
-	if aDuration < bDuration {
-		return a
+// 	if aDuration < bDuration {
+// 		return a
+// 	}
+
+// 	return b
+// }
+
+func ConfigurableRacer(a, b string, timeout time.Duration) (winner string, error error) {
+	select {
+	case <- ping(a):
+		return a, nil
+	case <- ping(b):
+		return b, nil
+	case <- time.After(timeout):
+		return "", fmt.Errorf("timed out waiting for %s and %s", a, b)
 	}
-
-	return b
 }
 
-func measureResponseTime(url string) time.Duration {
-	start := time.Now()
-	http.Get(url)
-	return time.Since(start)
+const tenSecondTimeout = 10 * time.Second
+
+func Racer(a, b string) (winner string, error error) {
+	return ConfigurableRacer(a, b, tenSecondTimeout)
 }
+
+func ping(url string) chan struct{} {
+	ch := make(chan struct{}) // empty struct occupies no memory
+	go func() {
+		http.Get(url)
+		close(ch)
+	}()
+	return ch
+}
+// We have defined a function ping which creates a chan struct{} and returns it.
+// In our case, we don't care what type is sent to the channel, we just want to signal we are done and closing the channel works perfectly!
+// Why struct{} and not another type like a bool? Well, a chan struct{} is the smallest data type available from a memory perspective so we get no allocation versus a bool. Since we are closing and not sending anything on the chan, why allocate anything?
+// Inside the same function, we start a goroutine which will send a signal into that channel once we have completed http.Get(url).
+
+// func measureResponseTime(url string) time.Duration {
+// 	start := time.Now()
+// 	http.Get(url)
+// 	return time.Since(start)
+// }
--- 12-select/racer_test.go
+++ 12-select/racer_test.go
@@ -18,26 +18,41 @@ func TestSelect(t *testing.T) {
 	// 	t.Errorf("got %q, want %q", got, want)
 	// }
 	
-	slowServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		time.Sleep(20 * time.Millisecond)
+	t.Run("returns the url of the fastest server", func(t *testing.T){
+		slowServer := makeDelayedServer(20 * time.Millisecond)
+		fastServer := makeDelayedServer(0 * time.Millisecond)
+
+		defer slowServer.Close()
+		defer fastServer.Close()
+
+		slowUrl := slowServer.URL
+		fastUrl := fastServer.URL
+
+		want := fastUrl
+
+		got, _ := Racer(slowUrl, fastUrl)
+
+		if got != want {
+			t.Errorf("got %q, want %q", got, want)
+		}
+	})
+
+	t.Run("returns an error if a server doesn't respond within 10s", func(t *testing.T){
+		server := makeDelayedServer(25 * time.Second)
+
+		defer server.Close()
+
+		_, err := ConfigurableRacer(server.URL, server.URL, 20 * time.Millisecond)
+
+		if err == nil {
+			t.Error("expected an error but didn't get one")
+		}
+	})
+}
+
+func makeDelayedServer(delay time.Duration) *httptest.Server {
+	return httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(delay)
 		w.WriteHeader(http.StatusOK)
 	}))
-	
-	fastServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request){
-		w.WriteHeader(http.StatusOK)
-	}))
-	
-	defer slowServer.Close()
-	defer fastServer.Close()
-	
-	slowUrl := slowServer.URL
-	fastUrl := fastServer.URL
-
-	want := fastUrl
-
-	got := Racer(slowUrl, fastUrl)
-
-	if got != want {
-		t.Errorf("got %q, want %q", got, want)
-	}
 }
